1deca6073e75683fc168d1a13de3ac66b6980a74
kidiga <juniorprogrammer09@gmail.com>
2025-01-09 18:50:10 +0300

    added kotlin serialization and navigation libs and update the target and compile Sdk versions to 25 for android studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,18 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 android {
     namespace = "io.kdbrian.urbandict"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.kdbrian.urbandict"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -56,4 +58,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //serialization
+    implementation(libs.kotlinx.serialization.json)
 }

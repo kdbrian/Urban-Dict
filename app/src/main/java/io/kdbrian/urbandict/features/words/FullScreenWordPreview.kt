f193464f68f4c288147a56566e0200304784638f
kidiga <juniorprogrammer09@gmail.com>
2025-01-09 22:10:10 +0300

    full screen word preview sreen

---
+++ app/src/main/java/io/kdbrian/urbandict/features/words/FullScreenWordPreview.kt
@@ -0,0 +1,11 @@
+package io.kdbrian.urbandict.features.words
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FullScreenWordPreview(
+    modifier: Modifier = Modifier,
+
+){
+}

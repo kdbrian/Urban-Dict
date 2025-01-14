884454c21aab362502d5c86ef899f7ee58848859
kidiga <juniorprogrammer09@gmail.com>
2025-01-14 14:51:21 +0300

    added a supported constructor

--- app/src/main/java/io/kdbrian/urbandict/data/model/UrbanWord.kt
+++ app/src/main/java/io/kdbrian/urbandict/data/model/UrbanWord.kt
@@ -4,11 +4,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class UrbanWord(
-    val wordId: String,
-    val word: String,
-    val author: String,
-    val definition: String,
-    val acronyms: List<String>,
-    val usedBy: List<String>,
-    val banned: Boolean
-)
+    val wordId: String = "",
+    val word: String = "",
+    val author: String = "",
+    val definition: String = "",
+    val acronyms: List<String> = emptyList(),
+    val usedBy: List<String> = emptyList(),
+    val banned: Boolean = false,
+) {
+    constructor(word: String, definition: String, author: String) : this(wordId = "", word = word, definition = definition, author = author)
+
+}
